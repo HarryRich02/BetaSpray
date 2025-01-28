bd223e81586af16f59178d9fff6592b2cf371b4c
HarryRich02 <klvr36@durham.ac.uk>
2025-01-28 15:39:17 +0000

    idek anymore

--- static/index.js
+++ static/index.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+function init() {
     const content = document.getElementById('content');
 
     document.querySelectorAll('a[data-file]').forEach((link) => {
@@ -16,11 +16,24 @@ document.addEventListener('DOMContentLoaded', () => {
                             document.getElementById('routeStatic').clientHeight;
                         const comments =
                             document.getElementById('betaComments');
-                        console.log(infoHeight);
                         comments.style.maxHeight = `${imgHeight - infoHeight}px`;
+                    } else if (file === 'pages/overview.html') {
+                        init();
                     }
                 });
         });
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    init();
+
+    const addClimbModal = new bootstrap.Modal(document.getElementById('modalAddClimb'));
+    addClimbModal.hide();
+
+    const newClimbButton = document.getElementById('newClimb')
+    newClimbButton.addEventListener('click', () => {
+        addClimbModal.show()
     });
 });
 
